f65b0f4ecd5cce28b0c3f46d3503e2cca7aa07cb
Stephen Bafico <stephenbafico@gmail.com>
2015-07-03 19:25:07 -0700

    Add support for Japanese text in the Game Name

--- mdheader.c
+++ mdheader.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include <stdint.h>
 #include <endian.h>
+#include <iconv.h>
+#include <stdlib.h>
 
 #define HEADER_SIZE 0x100
 #define HEADER_OFFSET 0x100
@@ -45,7 +47,6 @@ char *regions[] =
     "Japan",
     "USA",
     "Europe",
-    "Unknown Region",
 };
 
 char *io2string(char io)
@@ -78,6 +79,7 @@ char *io2string(char io)
 
 char *region2string(char region)
 {
+    static char unk[] = "Unknown Region 'X'";
     switch (region) {
         case 'J' :
             return regions[0];
@@ -86,7 +88,8 @@ char *region2string(char region)
         case 'E' :
             return regions[2];
         default:
-            return regions[3];
+            unk[16] = region;
+            return unk;
     }
 }
 
@@ -108,10 +111,19 @@ int main(int argc, char *argv[])
     char buff[HEADER_SIZE];
     struct mdheader *mdh;
     char str[49];
+    int inb = 48, outb=128;
+    char *inm, *outm, *in_orig, *out_orig;
     int i;
+    iconv_t id;
 
     if (argc != 2)
         return 1;
+
+    id = iconv_open("UTF-8", "SHIFT_JIS");
+    if (id == (iconv_t)(-1)) {
+        printf("bad iconv_open\n");
+        return 1;
+    }
 
     rom = fopen(argv[1], "r");
     if (!rom)
@@ -130,37 +142,59 @@ int main(int argc, char *argv[])
     }
 
     mdh = (struct mdheader *)buff;
+    inm = in_orig = malloc(48);
+    memcpy(inm, mdh->title_dom, 48);
+    outm = out_orig = malloc(128);
+    if (!inm || !outm)
+        return 1;
 
     printf("************************************************\n");
     printf("Common title:\n%s\n", term(mdh->title_com, str, 16));
     printf("Copyright:\n%s\n", term(mdh->copyright, str, 8));
     printf("Date:\n%s\n", term(mdh->date, str, 8));
-    printf("Game name (domestic):\n%s\n", term(mdh->title_dom, str, 48));
-    printf("Game name (overseas):\n%s\n", term(mdh->title_over, str, 48));
+
+    printf("Game name (domestic):\n");
+    ret = iconv(id, &inm, &inb, &outm, &outb);
+    if (ret >= 0)
+        printf("%s\n", out_orig);
+    else
+        printf("conversion failure\n");
+    printf("Game name (overseas):\n");
+    inm = in_orig;
+    outm = out_orig;
+    inb = 48;
+    outb = 128;
+    ret = iconv(id, &inm, &inb, &outm, &outb);
+    if (ret >= 0)
+        printf("%s\n", out_orig);
+    else
+        printf("conversion failure\n");
+
     printf("Type:\n%s\n", term(mdh->type, str, 2));
     printf("Product:\n%s\n", term(mdh->product, str, 14));
-    //printf("Controls:\n%s\n", term(mdh->controls, str, 16));
     printf("Controls:\n");
     for (i = 0; i < 16; i++) {
         if(mdh->controls[i] == ' ')
             continue;
         printf("%s\n", io2string(mdh->controls[i]));
     }
+
     printf("ROM Start:\n0x%x\n", be32toh(mdh->rom_start));
     printf("ROM End:\n0x%x\n",  be32toh(mdh->rom_end));
     printf("RAM Start:\n0x%x\n", be32toh(mdh->ram_start));
     printf("RAM End:\n0x%x\n",  be32toh(mdh->ram_end));
+
     printf("Modem:\n%s\n", term(mdh->modem, str, 12));
     printf("Memo:\n%s\n", term(mdh->memo, str, 40));
-    //printf("Regions:\n%s\n", term(mdh->regions, str, 16));
     printf("Regions:\n");
     for (i = 0; i < 16; i++) {
-        if(mdh->controls[i] == ' ')
+        if(mdh->regions[i] == ' ')
             continue;
         printf("%s\n", region2string(mdh->regions[i]));
     }
     printf("************************************************\n");
 
     fclose(rom);
+    iconv_close(id);
     return 0;
 }
